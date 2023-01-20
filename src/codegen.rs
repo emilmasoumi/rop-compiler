4555020a76e7b2ef3ae6b4b56c14d6c1d2de8f5b
Emil Masoumi <emilmasoumi@protonmail.com>
2023-01-20 20:51:08 +0100

    Update.
    Signed-off-by: Emil Masoumi <emilmasoumi@protonmail.com>

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,168 +1,247 @@
-use capstone::prelude::*;
+use self::Pos::*;
+use self::Exp::*;
+use self::AST::*;
+use self::Const::*;
 
-use std::process::Command;
-use std::io::{self, Write};
+use capstone::{prelude::*, Instructions};
+use keystone_engine::*;
+use object::{Object, ObjectSection, Endianness};
 
+use ast::{Pos};
 use ast::*;
-use utils::{is_hex};
-
-use self::Arch::*;
-use self::ArchSize::*;
-use self::AsmSyntax::*;
-use self::CPUType::*;
-use self::Endianess::*;
-
-#[derive(Debug)]
-pub enum Arch {
-  ArchARM,
-  ArchX86,
-}
-
-#[derive(Debug)]
-pub enum ArchSize {
-  BitSize8,
-  BitSize16,
-  BitSize32,
-  BitSize64,
-}
-
-#[derive(Debug)]
-pub enum AsmSyntax {
-  Att,
-  Intel,
-}
-
-// rasm2 CPU types
-#[derive(Debug)]
-pub enum CPUType {
-  NoCPUType,
-  V8,
-  Cortex,
-  R6,
-  V3,
-  V2,
-  V9,
-}
-
-#[derive(Debug)]
-pub enum Endianess {
-  Lsb,
-  Msb,
-}
-
-static ARCHS:     &[&str] = &["arm", "x86"];
-static ARCHSIZES: &[&str] = &["8", "16", "32", "64"];
-static CPUTYPES:  &[&str] = &["", "v8", "cortex", "r6", "v3", "v2", "v9"];
-static ENDIAN:    &[&str] = &["", "-e"];
-static SYNTAX:    &[&str] = &["att", "intel"];
-
-fn transform() {
-}
-
-fn assemble(code : String, arch_idx : usize, archsize_idx : usize,
-            endianess_idx : usize, cputype_idx : usize, syntax_idx : usize) -> String {
-  let mut options = vec![];
-  let arch        = ARCHS[arch_idx];
-  let archsize    = ARCHSIZES[archsize_idx];
-  let cputype     = CPUTYPES[cputype_idx];
-  let endianess   = ENDIAN[endianess_idx];
-  let syntax      = SYNTAX[syntax_idx];
-
-  options.push("-a");
-  options.push(arch);
-  options.push("-b");
-  options.push(archsize);
-  options.push("-s");
-  options.push(syntax);
-  if !cputype.is_empty() {
-    options.push("-c");
-    options.push(cputype);
-  }
-  if !endianess.is_empty() {
-    options.push(endianess);
-  }
-  options.push(&code);
-
-  let opcodes = if cfg!(target_os = "windows") {
-    Command::new("start C:\rasm2.exe")
-            .args(&options)
-            .output()
-            .expect("failed executing a process")
-  } else {
-    Command::new("/bin/rasm2")
-            .args(&options)
-            .output()
-            .expect("failed executing a process")
-  };
-
-  let failure     = String::from_utf8_lossy(&opcodes.stderr);
-  let opcodes_str = String::from_utf8_lossy(&opcodes.stdout).split_whitespace().collect();
-
-  if failure.len() > 0 || !opcodes.status.success() {
-    let sh_cmd = if cfg!(target_os = "windows") {
-      "start C:\rasm2.exe "
-    }
-    else {
-      "/bin/rasm2 "
-    };
-    error!("assembling:\n", code, "\nfailed with output:\n", failure, opcodes_str,
-           "\nfrom the following shell command:\n", sh_cmd, options.join(" "));
-  }
-
-  if !is_hex(&opcodes_str) {
-    error!("the opcodes:\n", opcodes_str, "\nreturned from assembling:\n", code,
-           "\nis not of hexadecimal format.");
-  }
-
-  opcodes_str
-}
 
 fn read_bytes(bin : &str) -> Vec<u8> {
   match std::fs::read(bin) {
-    Ok(bytes) => {
-      bytes
+    Ok(bytes) => { bytes }
+    Err(e)    => { error!("failed reading from: \'", bin, "\': ", e) }
+  }
+}
+
+fn pp_pos(pos : &Pos) -> String {
+  let (l, c) = match pos { Pos(x, y) => (x, y) };
+  string!(l) + ":" + &string!(c)
+}
+
+fn kmp_table(needle : &[u8]) -> Vec<usize> {
+  let mut table : Vec<usize> = vec![0; needle.len()];
+  let mut j = 0;
+
+  for i in 1..needle.len() {
+    if needle[i] == needle[j] { table[i] = table[j]; }
+    else {
+      table[i] = j;
+      while j > 0 && needle[i] != needle[j] { j = table[j]; }
     }
-    Err(e) => {
-      error!("failed reading from: \'", bin, "\': ", e);
+    j += 1;
+  }
+
+  table
+}
+
+fn kmp(haystack : &[u8], needle : &[u8]) -> Option<usize> {
+  let (mut i, mut j, mut _idx, table) = (0, 0, 0, kmp_table(needle));
+  //let mut idxs = Vec::new();
+
+  while i < haystack.len() {
+    if needle[j] == haystack[i] {
+      i += 1;
+      j += 1;
+      if j == needle.len() {
+        return Some(i-j);
+        //idxs.insert(idx, i-j);
+        //idx += 1;
+        //j = table[j];
+      }
+    }
+    else {
+      j = table[j];
+      if j == 0 {
+        i += 1;
+        j += 1;
+      }
+    }
+  }
+
+  None
+  //idxs
+}
+
+fn assemble(engine : &Keystone, code : String, pos : &Pos) -> Vec<u8> {
+  engine
+    .asm(own!(code), 0)
+    .expect(&format!("error: {} assembler: {}", pp_pos(pos), code))
+    .bytes
+}
+
+fn cs(archcpu : (Arch, Mode), syntax : arch::x86::ArchSyntax) -> Capstone {
+  let err_msg = format!("Failed creating the Capstone object for: {:?}", archcpu);
+  // `Arch` values must be supplied in comparisons as the `Mode` values overlap.
+  match archcpu {
+    (Arch::ARM, Mode::ARM) => {
+      Capstone::new().arm().mode(arch::arm::ArchMode::Arm).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::ARM, Mode::THUMB) => {
+      Capstone::new().arm().mode(arch::arm::ArchMode::Thumb).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::MIPS, Mode::MIPS3) => {
+      Capstone::new().mips().mode(arch::mips::ArchMode::Mips3).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::MIPS, Mode::MIPS32R6) => {
+      Capstone::new().mips().mode(arch::mips::ArchMode::Mips32R6).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::MIPS, Mode::MIPS32) => {
+      Capstone::new().mips().mode(arch::mips::ArchMode::Mips32).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::MIPS, Mode::MIPS64) => {
+      Capstone::new().mips().mode(arch::mips::ArchMode::Mips64).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::SPARC, Mode::SPARC32 | Mode::SPARC64) => {
+      Capstone::new().sparc().mode(arch::sparc::ArchMode::Default).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::SPARC, Mode::V9) => {
+      Capstone::new().sparc().mode(arch::sparc::ArchMode::V9).detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::X86, Mode::MODE_16) => {
+      Capstone::new().x86().mode(arch::x86::ArchMode::Mode16).syntax(syntax)
+                     .detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::X86, Mode::MODE_32) => {
+      Capstone::new().x86().mode(arch::x86::ArchMode::Mode32).syntax(syntax)
+                     .detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::X86, Mode::MODE_64) => {
+      Capstone::new().x86().mode(arch::x86::ArchMode::Mode64).syntax(syntax)
+                     .detail(true).build()
+                     .expect(&err_msg)
+    }
+    (Arch::ARM, Mode::V8) | (Arch::MIPS, Mode::MICRO) => {
+      /*
+      Capstone::new().arm().extra_mode(arch::arm::ArchExtraMode::V8).detail(true)
+      .build().expect(&err_msg)
+      */
+      error!("The CPU modes: V8 and Micro are not yet supported.")
+    }
+    a => {
+      error!("Unknown CPU type given: ", format!("{:?}", a))
     }
   }
 }
 
-fn byte_to_nibbles(byte : u8) -> (u8, u8) {
-  ((byte >> 4) & 0b1111, byte & 0b1111)
-}
-
-pub fn codegen(ast : &[AST], bins : Vec<String>, arch : Arch,
-               archsize : ArchSize, endianess : Endianess, syntax : AsmSyntax,
-               cputype : CPUType) -> String {
-  codegen_(ast, bins, arch as usize, archsize as usize, endianess as usize,
-           syntax as usize, cputype as usize)
-}
-
-/* Dummy code for now. */
-fn codegen_(ast : &[AST], bins : Vec<String>, arch : usize, archsize : usize,
-            endianess : usize, syntax : usize, cputype : usize) -> String {
-  assemble("mov rax, rax; mov rax, rax;".to_string(),
-           arch , archsize, endianess, cputype, syntax);
-//  println!("{}", assemble("mov rax, rax; mov rax, rax;".to_string(),
-//           arch , archsize, endianess, cputype, syntax));
-//  println!("{}", assemble("add rsi, rcx;".to_string(),
-//           arch , archsize, endianess, cputype, syntax));
-
-  // Only the main program is considered for now.
-  let prog_opcodes = read_bytes(&bins[0]);
-
-  // Find memory addresses of the instruction locations in the given `bins` for each byte or
-  // nibble in a sequence of opcodes.
-
-  let mut cnt = 0;
-  for x in &prog_opcodes {
-    let (nib1, nib2) = byte_to_nibbles(*x);
-    if nib1 == 0xc && nib2 == 0x3 {
-      cnt += 1;
+fn get_opcodes_addr_offs(obj : object::File<'_>) -> (&[u8], u64) {
+  // We currently do not wish to consider other sections.
+  let sections = [".text", "__text"];
+  for s in sections {
+    if let Some(section) = obj.section_by_name(s) {
+      if let Ok(opcodes) = section.data() { return (opcodes, section.address()) }
+      else { error!("Failed to get the opcodes for the ", s, " section.") }
     }
   }
-  //println!("cnt = {}", cnt);
+  error!("Failed to find an executable section.")
+}
 
-  string!("")
+#[inline(always)]
+fn no_gadget_err(gadget : &[Const]) -> ! {
+  let mut g = gadget.iter().map(|e|
+    match e {
+      Asm(asm, _, _) => asm.to_owned() + "\n",
+    }
+  ).collect::<String>();
+  g.pop();
+  error!("Failed to find a memory address for the gadget(s):\n", g);
+}
+
+fn mnemonicwise_search(gadget : &[Const], insns : &Instructions<'_>,
+                       engine : &Keystone) -> String {
+  for g in gadget {
+    match g {
+      Asm(asm, pos, _) => {
+        let obj_code = assemble(engine, own!(asm), pos);
+        for ins in insns.as_ref() {
+          if obj_code.iter().eq(ins.bytes().iter()) {
+            return string!(ins.address())
+          }
+        }
+      },
+    }
+  }
+  no_gadget_err(gadget)
+}
+
+fn bytewise_search(addr_offs : u64, gadget : &[Const], opcodes : &[u8],
+                   engine : &Keystone) -> String {
+  for g in gadget {
+    match g {
+      Asm(asm, pos, _) => {
+        let obj_code = assemble(engine, own!(asm), pos);
+        if let Some(gadget_offs) = kmp(opcodes, &obj_code) {
+          return string!(addr_offs + gadget_offs as u64)
+        }
+      },
+    }
+  }
+  no_gadget_err(gadget)
+}
+
+fn eval_gadget(opcodes : &[u8], gadget : &[Const], insns : &Instructions<'_>,
+               engine : &Keystone, bytewise : bool,
+               addr_offset : u64, outind : bool) -> String {
+  let addr = if bytewise {
+    bytewise_search(addr_offset, gadget, opcodes, engine)
+  }
+  else {
+    mnemonicwise_search(gadget, insns, engine)
+  };
+  if outind { addr + "\n" }
+  else      { addr        }
+}
+
+pub fn codegen(ast : &[AST], bin : String, archcpu : (Arch, Mode),
+               syntax : (OptionValue, arch::x86::ArchSyntax),
+               bytewise : bool, outind : bool) -> String {
+  let data = read_bytes(&bin);
+
+  let engine = Keystone::new(archcpu.0, archcpu.1)
+                        .expect("Failed initializing the Keystone engine");
+
+  if archcpu.0 == Arch::X86 {
+    engine.option(OptionType::SYNTAX, syntax.0)
+          .expect(&own!(format!("Could not set the x86 assembly syntax to: {:?}", syntax.0)));
+  }
+
+  let obj = match object::File::parse(&*data) {
+    Ok(obj) => { obj }
+    Err(e)  => { error!(format!("Failed to parse {} into object code: {}", bin, e)) }
+  };
+
+  let endianess = obj.endianness();
+  if endianess == Endianness::Big {
+    error!("big-endian architectures are not yet supported.");
+  }
+
+  let (opcodes, addr_offset) = get_opcodes_addr_offs(obj);
+
+  let cs        = cs(archcpu, syntax.1);
+  let mut insns = cs.disasm_all(&[], addr_offset)
+                    .expect("Failed to disassemble");
+  if !bytewise {
+    insns = cs.disasm_all(opcodes, addr_offset)
+              .expect("Failed to disassemble");
+  }
+
+  ast.iter().map(|n| {
+    match n {
+      Stat(Gadget(g)) => eval_gadget(opcodes, g, &insns, &engine, bytewise, addr_offset, outind),
+      _ => string!("")
+    }
+  }).collect()
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,14 @@
-use clap::{Arg, App};
-
 use ast::{Pos};
-use codegen::{Arch, ArchSize, AsmSyntax, CPUType, Endianess};
+
+use clap::{Arg, App};
+use capstone::prelude::arch::*;
+use keystone_engine::*;
 
 use std::io::prelude::*;
 use std::fs::File;
-
 use std::fmt::Debug;
 
 use self::Pos::*;
-use self::Arch::*;
-use self::ArchSize::*;
-use self::AsmSyntax::*;
-use self::CPUType::*;
-use self::Endianess::*;
 
 macro_rules! error {
   ($($args:expr),*) => {{
@@ -29,6 +24,7 @@ macro_rules! error {
 macro_rules! u_e { ($($args:tt)*) => { error!("cmd: ", $($args)*) }; }
 
 macro_rules! string        { ($exp:expr) => { $exp.to_string()     }; }
+macro_rules! own           { ($exp:expr) => { $exp.to_owned()      }; }
 macro_rules! is_whitespace { ($exp:expr) => { $exp.is_whitespace() }; }
 
 macro_rules! as_str { ($id:ident) => { $id.as_str() }; }
@@ -79,157 +75,111 @@ pub fn pp_vec<T : Debug>(v : Vec<T>) {
   v.iter().for_each(|e| println!("{:?}", e));
 }
 
-fn get_src_bin(mut srcs : Vec<String>) -> (String, Vec<String>) {
+fn get_src_bin(mut srcs : Vec<String>) -> (String, String) {
   let mut src : String = String::new();
 
   let cnt = srcs.iter().fold(0, |acc, s|
       if s.ends_with(".rop") {src = s.clone(); acc + 1} else {acc});
 
   if cnt != 1 {
-    error!("expected exactly 1 source file with extension `.rop`, but got ",
+    error!("expected exactly 1 source file with the extension `.rop`, but got ",
            cnt, " instead: ", pp!(srcs));
   }
-  else if srcs.len() < 2 {
-    error!("expected at least 1 binary file, but got 0 instead: ", pp!(srcs));
+  else if srcs.len() != 2 {
+    error!("expected exactly 1 binary file, but got ", srcs.len()-cnt,
+           " instead: ", pp!(srcs));
   }
 
   srcs.retain(|x| !x.ends_with(".rop"));
-  (src, srcs)
+  (src, own!(srcs[0]))
 }
 
-pub fn parse_cmd_args() -> (String, Vec<String>, Arch, ArchSize, Endianess,
-                            AsmSyntax, CPUType, Option<String>, bool, bool) {
+pub fn parse_cmd_args() -> (String, String, (Arch, Mode),
+                            (OptionValue, x86::ArchSyntax), bool, bool) {
   let matches =
       App::new("ROP and JOP compiler")
       .version("0.0.1")
-      .about("Return-oriented programming and jump-oriented programming compiler.")
+      .about("A generic gadget chain compiler that supports the \
+              architectures: ARM/ARM64, MIPS32/64, SPARC32/64, x86/x64, and \
+              file formats: ELF, PE, Mach-O.")
       /* arguments: */
-      .arg(Arg::with_name("binaries")
+      .arg(Arg::with_name("files")
            .required(true)
            .multiple(true)
-           .help("Binary file(s)")
+           .help("A `.rop` file and a binary executable.")
           )
-      .arg(Arg::with_name("architecture")
-           .short("a")
-           .long("arch")
+      .arg(Arg::with_name("cpu-type")
+           .short('c')
+           .long("cputype")
            .required(true)
            .takes_value(true)
-           .possible_values(&["arm", "x86"])
-           .help("Instruction set architecture.")
-          )
-      .arg(Arg::with_name("architecture-size")
-           .short("b")
-           .long("archsize")
-           .required(true)
-           .takes_value(true)
-           .possible_values(&["8", "16", "32", "64"])
-           .help("Architecture bit-size.")
-          )
-      .arg(Arg::with_name("endianess")
-           .short("e")
-           .long("endianess")
-           .required(true)
-           .takes_value(true)
-           .possible_values(&["lsb", "msb"])
-           .help("Specifies the endianess of the binaries.")
-          )
-      .arg(Arg::with_name("assembly-syntax")
-           .short("s")
-           .long("select")
-           .required(true)
-           .takes_value(true)
-           .possible_values(&["att", "intel"])
-           .help("Select the assembly syntax.")
+           .possible_values(&["arm", "thumb", "armv8", "micro", "mips3",
+                              "mips32r6", "mips32", "mips64",
+                              "sparc32", "sparc64", "sparcv9",
+                              "x86-16", "x86-32", "x86-64"])
+           .help("Computer architecture/CPU type.")
           )
       /* options: */
-      .arg(Arg::with_name("cpu-type")
-           .short("c")
-           .long("cpu")
+      .arg(Arg::with_name("assembly-syntax")
+           .short('s')
+           .long("syntax")
+           .required_ifs(&[("cpu-type", "x86-16"),
+                           ("cpu-type", "x86-32"),
+                           ("cpu-type", "x86-64")])
            .takes_value(true)
-           .possible_values(&["v8", "cortex", "r6", "v3", "v2", "v9"])
-           .help("Select a specific CPU type.")
+           .possible_values(&["att", "gas", "intel", "nasm"])
+           .help("The assembly syntax for dis/assembling.")
           )
-      .arg(Arg::with_name("delete-bytes")
-           .short("d")
-           .long("delete")
-           .takes_value(true)
-           .help("Delete all occurrences of the given byte from the payload.")
-          )
-      .arg(Arg::with_name("insert-hex-sequences")
-           .short("H")
-           .long("hex")
+      .arg(Arg::with_name("byte-wise")
+           .short('b')
+           .long("bytewise")
            .takes_value(false)
-           .help("Insert hexadecimal escape sequences for every byte in the payload.")
+           .help("Search for memory addresses byte-wise instead of mnemonic-wise.")
           )
-      .arg(Arg::with_name("pp-gadgets")
-           .short("p")
-           .long("ppgadgets")
+      .arg(Arg::with_name("out-ind")
+           .short('o')
+           .long("individually")
            .takes_value(false)
-           .help("Pretty-print the gadget chain.")
+           .help("Output the addresses in the gadget chain individually.")
           )
       .get_matches();
 
   let files : Vec<_> =
-    matches.values_of("binaries")
-           .unwrap()
-           .map(|s| string!(s))
-           .collect();
+    matches.values_of("files").unwrap().map(|s| own!(s)).collect();
 
-  let arch : Arch =
-    match matches.value_of("architecture") {
-      Some("arm")   => ArchARM,
-      Some("x86")   => ArchX86,
-      _             => u_e!("The instruction set architecture were not specified."),
-    };
-
-  let archsize : ArchSize =
-    match matches.value_of("architecture-size") {
-      Some("8")  => BitSize8,
-      Some("16") => BitSize16,
-      Some("32") => BitSize32,
-      Some("64") => BitSize64,
-      _          => u_e!("The instruction set architecture bit-size were not specified."),
-    };
-
-  let endianess : Endianess =
-    match matches.value_of("endianess") {
-      Some("lsb") => Lsb,
-      Some("msb") => Msb,
-      _           => u_e!("Endianess of the given binaries were not specified."),
-    };
-
-  let syntax : AsmSyntax =
-    match matches.value_of("assembly-syntax") {
-      Some("intel") => Intel,
-      _             => Att,
-    };
-
-  let cputype : CPUType =
+  let cputype =
     match matches.value_of("cpu-type") {
-      Some("v8")     => V8,
-      Some("cortex") => Cortex,
-      Some("r6")     => R6,
-      Some("v3")     => V3,
-      Some("v2")     => V2,
-      Some("v9")     => V9,
-      _              => NoCPUType,
+      Some("arm")      => (Arch::ARM,   Mode::ARM),
+      Some("thumb")    => (Arch::ARM,   Mode::THUMB),
+      Some("armv8")    => (Arch::ARM,   Mode::V8),
+      Some("mips2")    => (Arch::MIPS,  Mode::MICRO),
+      Some("mips3")    => (Arch::MIPS,  Mode::MIPS3),
+      Some("mips32r6") => (Arch::MIPS,  Mode::MIPS32R6),
+      Some("mips32")   => (Arch::MIPS,  Mode::MIPS32),
+      Some("mips64")   => (Arch::MIPS,  Mode::MIPS64),
+      Some("sparc32")  => (Arch::SPARC, Mode::SPARC32),
+      Some("sparc64")  => (Arch::SPARC, Mode::SPARC64),
+      Some("sparcv9")  => (Arch::SPARC, Mode::V9),
+      Some("x86-16")   => (Arch::X86,   Mode::MODE_16),
+      Some("x86-32")   => (Arch::X86,   Mode::MODE_32),
+      Some("x86-64")   => (Arch::X86,   Mode::MODE_64),
+      _                => u_e!("The computer architecture/\
+                                CPU type was not specified."),
     };
 
-  let del_bytes : Option<String> =
-    matches.value_of("delete-bytes").map(|s| string!(s));
+  let syntax =
+    match matches.value_of("assembly-syntax") {
+      Some("intel") => (OptionValue::SYNTAX_INTEL, x86::ArchSyntax::Intel),
+      Some("nasm")  => (OptionValue::SYNTAX_NASM,  x86::ArchSyntax::Intel),
+      Some("att")   => (OptionValue::SYNTAX_ATT,   x86::ArchSyntax::Att),
+      Some("gas")   => (OptionValue::SYNTAX_GAS,   x86::ArchSyntax::Att),
+      _             => (OptionValue::SYNTAX_INTEL, x86::ArchSyntax::Intel),
+    };
 
-  if let Some(ref del_bytes) = del_bytes {
-    if !is_hex(del_bytes) {
-        error!("-d --delete \'", del_bytes, "\' is not a hexadecimal value.");
-    }
-  }
+  let bytewise = matches.is_present("byte-wise");
+  let outind   = matches.is_present("out-ind");
 
-  let hex       : bool = matches.is_present("insert-hex-sequences");
-  let ppgadgets : bool = matches.is_present("pp-gadgets");
+  let (src, bin) = get_src_bin(files);
 
-  let (src, bins) = get_src_bin(files);
-
-  (src, bins, arch, archsize, endianess, syntax,
-   cputype, del_bytes, hex, ppgadgets)
-
+  (src, bin, cputype, syntax, bytewise, outind)
 }

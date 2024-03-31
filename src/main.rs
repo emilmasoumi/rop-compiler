425997b853759f3874d0d3fcf19f19f385798777
Emil Masoumi <emilmasoumi@protonmail.com>
2024-03-31 23:50:01 +0200

    Update.
    Signed-off-by: Emil Masoumi <emilmasoumi@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,17 @@ mod utils;
 mod ast;
 mod parser;
 mod typechecker;
+mod ir;
 mod codegen;
 
 fn main() {
   let (src, bin, cputype, syntax, bitwidth, bytewise, byteorder, outind, all) =
     utils::parse_cmd_args();
 
-  let src = utils::read_file(&src);
-  let ast = parser::parser(&src);
+  let src     = utils::read_file(&src);
+  let mut ast = parser::parser(&src);
 
+  ir::ir(&mut ast);
   typechecker::typechecker(&src, &ast);
 
   let mut payload = codegen::codegen(&ast, bin, cputype, syntax, bitwidth,
